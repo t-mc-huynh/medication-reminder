113ade07c6fcfae5d950ac740e07e03bf3002a47
Tina Huynh <tinahuynh9898@gmail.com>
2019-03-01 20:36:40 -0800

    initial

---
+++ medication-reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  medication-reminder
+//
+//  Created by Tina Huynh on 3/1/19.
+//  Copyright © 2019 Tina Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
