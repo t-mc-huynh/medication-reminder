56c7e8ff86d3ce44d5fda5de9705c820bdfc7f87
John Lee <jlee14c@hotmail.com>
2019-03-02 16:59:46 -0800

    table

--- medication-reminder/Medications.swift
+++ medication-reminder/Medications.swift
@@ -11,34 +11,37 @@ import UIKit
 
 class Medications {
     // Responsible for managing the local database of medications
-    
-    var MOCK_MEDICATION_TUPLES: [(String, [Float], String, String, [String])]
+    var medications: [Medication] = []
+    var MOCK_MEDICATION_TUPLES: [(String, [Float], String, String, [String])] = [
+        ("Lorazepam (Ativan SL)", [0.5, 1, 2], "MG", "SL TAB", ["Avoid taking with alcohol", "Can cause drowsiness"]),
+        ("Zopiclone (Imovane)", [5.0, 7.5], "MG", "TAB", ["Avoid taking with alcohol"]),
+        ("Salbutamol Sulfate (Ventolin HFA)", [100], "MCRGM", "MDI", []),
+        ("Escitalopram oxalate (Cipralex)", [10, 20], "MG", "TAB", []),
+        ("Ramipril (Altact)", [1.25, 2.5, 5, 10], "MG", "CAP", []),
+        ("Rifampin (Rifadin)", [150, 300], "MG", "TAB", ["Avoid calcium, magnesium, aluminum, and iron", "Avoid dairy products", "Avoid taking antacids"])
+    ]
 
     public init () {
-        MOCK_MEDICATION_TUPLES = [
-            ("Lorazepam (Ativan SL)", [0.5, 1, 2], "MG", "SL TAB", ["Avoid taking with alcohol", "Can cause drowsiness"]),
-            ("Zopiclone (Imovane)", [5.0, 7.5], "MG", "TAB", ["Avoid taking with alcohol"]),
-            ("Salbutamol Sulfate (Ventolin HFA)", [100], "MCRGM", "MDI", []),
-            ("Escitalopram oxalate (Cipralex)", [10, 20], "MG", "TAB", []),
-            ("Ramipril (Altact)", [1.25, 2.5, 5, 10], "MG", "CAP", []),
-            ("Rifampin (Rifadin)", [150, 300], "MG", "TAB", ["Avoid calcium, magnesium, aluminum, and iron", "Avoid dairy products", "Avoid taking antacids"])
-        ]
+        var temp: Medication
+        for (a, b, c, d, e) in MOCK_MEDICATION_TUPLES {
+            temp = Medication(name: a, doses: b, dosageType: c, dosageForm: d, warnings: e)
+            self.medications.append(temp)
+        }
     }
     
-    public func getMockMedications() -> [(String, [Float], String, String, [String])] {
-        return MOCK_MEDICATION_TUPLES
+    public func getMedications() -> [Medication] {
+        return self.medications
     }
     
-    public func findMatchingMedications(searchTerm: String) ->  [(String, [Float], String, String, [String])] {
-        var result = [(String, [Float], String, String, [String])]()
+    public func findMatchingMedications(searchTerm: String) ->  [Medication] {
+        var result = [Medication]()
         
-        for medication in getMockMedications() {
-            if (medication.0.lowercased().range(of: searchTerm.lowercased()) != nil) {
+        for medication in getMedications() {
+            if (medication.getName().lowercased().range(of: searchTerm.lowercased()) != nil) {
                 result.append(medication)
             }
         }
         
         return result;
     }
-
 }
--- medication-reminder/ViewController.swift
+++ medication-reminder/ViewController.swift
@@ -8,26 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var MOCK_MEDICATION_TUPLES: [(String, [Float], String, String, [String])] = []
     var EXAMPLE_MEDICATION_RECORDS: [(String, [Float], String, String, [String], String, UIDatePicker, Int)] = []
+    var medicationsData: Medications = Medications()
+    var medicationsList: [Medication] = []
+//    var tableView: UITableView = UITableView()
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "drug_search_cell"
     
+    //MARK: properties
+//        @IBOutlet var tableView: UITableView!
+
+    @IBOutlet weak var drugSearchTable: UITableView!
+    @IBOutlet weak var drugSearchBar: UITextField!
+    @IBOutlet weak var medRecordTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register the table view cell class and its reuse id
+        self.drugSearchTable.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        // self.tableView.tableFooterView = UIView()
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        drugSearchTable.delegate = self
+        drugSearchTable.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.drugSearchTable.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.animals[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    //MARK: actions
     @IBAction func infoButton(_ sender: Any) {
         
     }
     
     @IBAction func medSearchInput(_ sender: UITextField) {
         
+        if (sender.text != nil) {
+            medicationsList = medicationsData.findMatchingMedications(searchTerm: sender.text ?? "")
+            
+            // TODO: Update Table with medicationsData
+        }
     }
-    @IBAction func medSearchButton(_ sender: UIButton) {
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
 
